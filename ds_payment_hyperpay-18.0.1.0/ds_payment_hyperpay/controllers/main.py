47732e8428d0c5e62d23724d4787eef190cdb978
saham911 <87824595+saham911@users.noreply.github.com>
2025-08-07 02:18:07 +0300

    Update main.py

--- ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/controllers/main.py
+++ ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/controllers/main.py
@@ -22,19 +22,17 @@ class HyperPayController(http.Controller):
     def hyperpay_return(self, **data):
         _logger.info("handling redirection from HyperPay with data:\n%s", pprint.pformat(data))
         request.env['payment.transaction'].sudo()._handle_notification_data('hyperpay', data)
-        return request.redirect('/my/orders')  # ✅ يعيد العميل إلى صفحة الطلبات بعد الدفع
+        return request.redirect('/payment/status')
 
     @http.route(_return_url_mada, type='http', auth='public', methods=['GET'], csrf=False, save_session=False)
     def hyperpay_return_mada(self, **data):
         _logger.info("handling redirection from HyperPay with data:\n%s", pprint.pformat(data))
         request.env['payment.transaction'].sudo()._handle_notification_data('mada', data)
-        return request.redirect('/my/orders')  # ✅ يعيد العميل إلى صفحة الطلبات بعد الدفع
+        return request.redirect('/payment/status')
 
     @http.route('/payment/hyperpay', website=True, type='http', auth='public', methods=['POST'], csrf=False, save_session=False)
     def hyperpay_redirect(self, **post_data):
         provider = post_data.get('paymentMethodCode', 'hyperpay')
-        base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        
         form_values = {
             'payment_url': post_data.get('payment_url', False),
             'checkout_id': post_data.get('checkout_id', False),
@@ -42,13 +40,7 @@ class HyperPayController(http.Controller):
             'provider': provider,
         }
         if provider == 'mada':
-            form_values.update({
-                'return_url': f'{base_url}{self._return_url_mada}',
-                'brands': 'MADA'
-            })
+            form_values.update({'return_url': self._return_url_mada, 'brands': 'MADA'})
         else:
-            form_values.update({
-                'return_url': f'{base_url}{self._return_url}',
-                'brands': 'VISA MASTER'
-            })
+            form_values.update({'return_url': self._return_url, 'brands': 'VISA MASTER'})
         return request.render('ds_payment_hyperpay.hyperpay_payment_form', form_values)
