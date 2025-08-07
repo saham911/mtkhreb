e657523468dce4ee5af52ff1636f60839bd197a3
saham911 <87824595+saham911@users.noreply.github.com>
2025-08-07 13:46:41 +0300

    Update __manifest__.py
    back

--- ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/__manifest__.py
+++ ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/__manifest__.py
@@ -1,35 +1,47 @@
-{
-    'name': "HyperPay Payment Gateway",
-    'version': '18.0.1.0',
-    'category': 'Accounting/Payment Providers',
-    'sequence': 51,
-    'summary': 'Integration with HyperPay payment gateway for MADA, Visa and MasterCard payments',
-    'description': """
-        Complete integration with HyperPay payment gateway
-        Supports MADA, Visa and MasterCard payments
-        Designed for Saudi Arabian market
-    """,
-    'author': 'DevSouls',
-    'support': "odoodevsouls@gmail.com",
-    'website': "https://www.dev-souls.com",
-    'images': ['static/description/banner.png'],
-    'depends': ['payment', 'website_sale'],
-    'data': [
-        'data/payment_provider_data.xml',
-        'views/payment_provider_views.xml',
-        'views/payment_hyperpay_templates.xml',
-    ],
-    'assets': {
-        'web.assets_frontend': [
-            'ds_payment_hyperpay/static/src/js/payment_form.js',
-        ],
-    },
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'price': 95.0,
-    'currency': 'USD'
-}
+<?xml version="1.0" encoding="utf-8"?>
+<odoo>
+    <record id="payment_method_hyperpay" model="payment.method">
+        <field name="name">HyperPay (VISA/Master Card)</field>
+        <field name="code">hyperpay</field>
+        <field name="sequence">4</field>
+         <field name="image" type="base64" file="ds_payment_hyperpay/static/description/visa.png"/>
+        <field name="support_tokenization">False</field>
+        <field name="support_express_checkout">False</field>
+        <field name="support_refund">none</field>
+        <field name="supported_currency_ids"
+               eval="[Command.set([
+                         ref('base.EUR'),ref('base.USD'),ref('base.SAR'),
+                     ])]"
+        />
+    </record>
+
+    <record id="payment_method_hyperpay_mada" model="payment.method">
+        <field name="name">HyperPay MADA</field>
+        <field name="code">mada</field>
+        <field name="sequence">5</field>
+         <field name="image" type="base64" file="ds_payment_hyperpay/static/description/mada.png"/>
+        <field name="support_tokenization">False</field>
+        <field name="support_express_checkout">False</field>
+        <field name="support_refund">none</field>
+        <field name="supported_currency_ids"
+               eval="[Command.set([
+                         ref('base.SAR'),
+                     ])]"
+        />
+    </record>
+
+    <record id="payment_provider_hyperpay" model="payment.provider">
+        <field name="name">HyperPay</field>
+        <field name="code">hyperpay</field>
+        <field name="image_128" type="base64" file="ds_payment_hyperpay/static/description/icon.png"/>
+        <field name="module_id" ref="base.module_ds_payment_hyperpay"/>
+        <field name="redirect_form_view_id" ref="ds_payment_hyperpay.hyperpay_redirect_form"/>
+        <field name="payment_method_ids"
+               eval="[Command.set([
+                         ref('ds_payment_hyperpay.payment_method_hyperpay'),
+                         ref('ds_payment_hyperpay.payment_method_hyperpay_mada')
+                     ])]"
+        />
+        <field name="pre_msg">You will be redirected to HyperPay payment page</field>
+    </record>
+</odoo>
