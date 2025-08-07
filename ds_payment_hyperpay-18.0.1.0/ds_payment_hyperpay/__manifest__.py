e21dd538f6804b3e51bd9df215e1eca707e0b556
saham911 <87824595+saham911@users.noreply.github.com>
2025-08-07 13:40:53 +0300

    Update __manifest__.py

--- ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/__manifest__.py
+++ ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/__manifest__.py
@@ -1,32 +1,35 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Copyright (C) 2024-TODAY
-#    Author: Odoo DevSouls <odoodevsouls@gmailcom>
-#
-#############################################################################
 {
-    'name': "Hyperpay Payment Gateway",
-    'version': '18.0.0.0',
+    'name': "HyperPay Payment Gateway",
+    'version': '18.0.1.0',
     'category': 'Accounting/Payment Providers',
     'sequence': 51,
-    'summary': 'Hyperpay Payment Gateway to intergrate Mada Card, Visa Card and Master Card payments. It supports Credit cards, Debit Cards and MADA Card payments in KSA, Saudi Arabia',
+    'summary': 'Integration with HyperPay payment gateway for MADA, Visa and MasterCard payments',
+    'description': """
+        Complete integration with HyperPay payment gateway
+        Supports MADA, Visa and MasterCard payments
+        Designed for Saudi Arabian market
+    """,
     'author': 'DevSouls',
-    'support': "odoodevsouls@gmailcom",
+    'support': "odoodevsouls@gmail.com",
+    'website': "https://www.dev-souls.com",
     'images': ['static/description/banner.png'],
-    'description': "Hyperpay Payment Gateway (Mada/Visa/Master), MADA Payments in KSA",
     'depends': ['payment', 'website_sale'],
     'data': [
+        'data/payment_provider_data.xml',
         'views/payment_provider_views.xml',
         'views/payment_hyperpay_templates.xml',
-        'data/payment_provider_data.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'ds_payment_hyperpay/static/src/js/payment_form.js',
+        ],
+    },
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
     'license': 'OPL-1',
     'installable': True,
+    'application': True,
     'auto_install': False,
-    'application': False,
     'price': 95.0,
     'currency': 'USD'
 }
