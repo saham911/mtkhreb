893ebf95251ea9776a75e5bdd218b1a1f2bbf13e
saham911 <87824595+saham911@users.noreply.github.com>
2025-08-09 19:34:42 +0300

    Update main.py

--- ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/controllers/main.py
+++ ds_payment_hyperpay-18.0.1.0/ds_payment_hyperpay/controllers/main.py
@@ -5,8 +5,10 @@
 #    Author: Odoo DevSouls <odoodevsouls@gmailcom>
 #
 #############################################################################
+
 import logging
 import pprint
+from werkzeug import urls
 
 from odoo import http
 from odoo.http import request
@@ -21,26 +23,58 @@ class HyperPayController(http.Controller):
     @http.route(_return_url, type='http', auth='public', methods=['GET'], csrf=False, save_session=False)
     def hyperpay_return(self, **data):
         _logger.info("handling redirection from HyperPay with data:\n%s", pprint.pformat(data))
+        # provider_code = 'hyperpay' (Visa/Master)
         request.env['payment.transaction'].sudo()._handle_notification_data('hyperpay', data)
         return request.redirect('/payment/status')
 
     @http.route(_return_url_mada, type='http', auth='public', methods=['GET'], csrf=False, save_session=False)
     def hyperpay_return_mada(self, **data):
-        _logger.info("handling redirection from HyperPay with data:\n%s", pprint.pformat(data))
+        _logger.info("handling redirection from HyperPay (MADA) with data:\n%s", pprint.pformat(data))
+        # provider_code = 'mada'
         request.env['payment.transaction'].sudo()._handle_notification_data('mada', data)
         return request.redirect('/payment/status')
 
     @http.route('/payment/hyperpay', website=True, type='http', auth='public', methods=['POST'], csrf=False, save_session=False)
     def hyperpay_redirect(self, **post_data):
-        provider = post_data.get('paymentMethodCode', 'hyperpay')
-        form_values = {
-            'payment_url': post_data.get('payment_url', False),
-            'checkout_id': post_data.get('checkout_id', False),
-            'amount': post_data.get('formatted_amount', False),
-            'provider': provider,
-        }
-        if provider == 'mada':
-            form_values.update({'return_url': self._return_url_mada, 'brands': 'MADA'})
+        """
+        يستقبل قيم الـ rendering من الموديل ويعرض صفحة الـ widget.
+        يتوقع الحقول:
+          - payment_url (رابط سكربت الـ widget مع checkoutId)
+          - checkout_id
+          - formatted_amount
+          - paymentMethodCode (hyperpay | mada)
+        """
+        _logger.info("render hyperpay widget with post_data:\n%s", pprint.pformat(post_data))
+
+        # تحقّق حدّ أدنى من الحقول
+        required = ['payment_url', 'checkout_id', 'formatted_amount', 'paymentMethodCode']
+        missing = [k for k in required if not post_data.get(k)]
+        if missing:
+            _logger.error("Missing required rendering values: %s", missing)
+            return request.not_found()
+
+        provider_code = (post_data.get('paymentMethodCode') or 'hyperpay').strip().lower()
+        if provider_code not in ('hyperpay', 'mada'):
+            provider_code = 'hyperpay'  # fallback آمن
+
+        # ابنِ return_url مطلق (absolute) لتفادي أي مشاكل إعادة توجيه
+        base = request.httprequest.host_url  # مثال: https://www.artcontracting.com/
+        if provider_code == 'mada':
+            return_url = urls.url_join(base, self._return_url_mada)
+            brands = 'MADA'
         else:
-            form_values.update({'return_url': self._return_url, 'brands': 'VISA MASTER'})
+            return_url = urls.url_join(base, self._return_url)
+            brands = 'VISA MASTER'
+
+        form_values = {
+            'payment_url': post_data.get('payment_url'),
+            'checkout_id': post_data.get('checkout_id'),
+            'amount': post_data.get('formatted_amount'),
+            'provider': provider_code,  # يستخدمه القالب في إبراز الشعارات
+            'return_url': return_url,
+            'brands': brands,
+        }
+
+        _logger.info("rendering ds_payment_hyperpay.hyperpay_payment_form with values:\n%s",
+                     pprint.pformat(form_values))
         return request.render('ds_payment_hyperpay.hyperpay_payment_form', form_values)
